512f38bd39b76def1949b92435f8bbd2a3998c1f
horiz.d <65459616+ABizCho@users.noreply.github.com>
2022-03-12 06:09:24 +0000

    recursive Review Test code

---
+++ DS_0_3_recursive.py
@@ -0,0 +1,18 @@
+# def recursive(n) :
+#   if n == 1:
+#     return 0
+#   print(n)
+#   return n * recursive(n)
+
+# recursive(3)
+
+def hello(n) :
+  if count == 0 :
+    return
+
+  print('Hello, world!',count)
+
+  hello(--count)
+
+hello(5)
+
